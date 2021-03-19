707f7c9237eddb947c2ba13f2343c955d21835ce
afaafa <afhazachra@gmail.com>
2021-03-19 21:45:34 +0700

    add tailwindcss

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const path = require('path');
 
 module.exports = {
@@ -12,13 +15,27 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
+        exclude: /node_modules/,
+        use: [ 
           {
             loader: 'style-loader',
           },
           {
-            loader: 'css-loader',
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
           },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          { 
+            loader: 'postcss-loader',
+          },
+          
         ],
       },
       {
@@ -30,12 +47,31 @@ module.exports = {
            }, 
         ],
       },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        // Set loaders to transform files.
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: 'fonts'
+            }
+          }
+        ]
+      }
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].bundle.css",
+      chunkFilename: "[id].css",
+    }),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/templates/index.html'),
       filename: 'index.html',
+      favicon: "./src/public/images/favicon.png",
     }),
     new CopyWebpackPlugin({
       patterns: [
@@ -44,6 +80,6 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
-    }),
+    }), 
   ],
 };
